198a461d9389cb6eb12bf282038ee82aaa662ecc
Ankan chakraborty <chakrabortyankan351@gmail.com>
2025-05-05 12:10:27 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,request,url_for 
+import pandas as pd
+import numpy as np 
+import pickle 
+scaler=pickle.load(open("models/scaled.pkl","rb"))
+svc=pickle.load(open("models/svc.pkl","rb"))
+application=Flask(__name__)
+app=application 
+
+@app.route("/",methods=['POST','GET'])
+def predict():
+    img_url_setosa=url_for('static', filename='photos/iris-setosa.png') 
+    img_url_versicolor=url_for('static', filename='photos/versicolor.png') 
+    img_url_virginica=url_for('static', filename='photos/virginica.png')
+    if request.method=="POST":
+        SL=float(request.form.get("sepal_length"))
+        SW=float(request.form.get("sepal_width"))
+        PL=float(request.form.get("petal_length"))
+        PW=float(request.form.get("petal_width"))
+        data=[[SL,SW,PL,PW]] 
+        Sdata=scaler.transform(data)
+        result=svc.predict(data)
+        print(result)
+        if result[0]==0:
+            return render_template("main.html",prediction="Iris Setosa",source=img_url_setosa)
+        elif result[0]==1:
+            return render_template("main.html",prediction="Iris Versicolor",source=img_url_versicolor)
+        elif result[0]==2:
+            return render_template("main.html",prediction="Iris Virginica",source=img_url_virginica)
+
+    return render_template("main.html") 
+
+if __name__=="__main__":
+    
+    app.run(debug=1)
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+This is a simple machine learning web app for predicting the class of an iris flower. 
+model used : Support Vector Classifier 
+framework : Flask
+code : Python 
+Details about iris dataset :
+:
+
+🌸 Iris Classification Prediction Model
+The Iris Classification Prediction Model is a machine learning project that uses supervised learning techniques to classify iris flowers into three species: Setosa, Versicolor, and Virginica, based on features such as sepal length, sepal width, petal length, and petal width.
+
+This classic dataset, introduced by Sir Ronald Fisher, is widely used as a beginner-friendly introduction to classification tasks in machine learning. In this project, we build and evaluate models like Logistic Regression, K-Nearest Neighbors, Decision Tree, and others to predict the species of iris flowers with high accuracy. The goal is to demonstrate end-to-end ML workflow: from data preprocessing and visualization to model training, evaluation, and prediction.
+
+
